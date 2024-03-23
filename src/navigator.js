19a5211f33813f537cf5ad2982f9e2ad3fd97379
Ashraf2004 <93571118+Ashraf2004@users.noreply.github.com>
2024-03-23 17:36:45 +0530

    Update navigator.js

--- src/navigator.js
+++ src/navigator.js
@@ -25,7 +25,7 @@ function NavigationBar() {
         <Nav className="ml-auto"> {/* Align the content to the right */}
           <Nav.Link className='adj' href="#home">Home</Nav.Link>
           <Nav.Link className='adj' href="#about">About</Nav.Link>
-          <Nav.Link className='adj' href="">Feedback</Nav.Link>
+          <Nav.Link className='adj' href="https://www.markuptag.com/demo/feedback-form-with-star-rating-html.php">Feedback</Nav.Link>
           <NavDropdown className='adj' title="Contact" id="basic-nav-dropdown">
             <NavDropdown.Item >managepro@gmail.com</NavDropdown.Item>
           </NavDropdown>
